7f7480e4f308bab3ebe29cf122641c180872bf4f
LexiconCode <thequietplace@protonmail.com>
2019-11-16 18:02:38 -0600

    Corrected import Caster v1.0.0
    Corrected import `dfplus.additions import IntegerRefST` - >`merge.additions import IntegerRefST`

--- bundles/Caster/python_voice_coding_plugin_caster_v1-0-0.py
+++ bundles/Caster/python_voice_coding_plugin_caster_v1-0-0.py
@@ -4,7 +4,7 @@ from castervoice.lib import control
 from castervoice.lib import settings
 from castervoice.lib.actions import Key, Text
 from castervoice.lib.context import AppContext
-from castervoice.lib.dfplus.additions import IntegerRefST
+from castervoice.lib.merge.additions import IntegerRefST
 from castervoice.lib.merge.state.short import R
 from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
 
