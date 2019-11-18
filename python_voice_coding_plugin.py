41826a6c21ca153fdfd4e160597b629a88ca9ed0
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2019-11-18 14:26:09 +0200

    Fix path error

--- python_voice_coding_plugin.py
+++ python_voice_coding_plugin.py
@@ -1,8 +1,16 @@
+import os
+import sys
+
 import sublime
 import sublime_plugin
 
+# making sure our dependencies are in the path
+sys.path.insert(0,os.path.join(os.path.dirname(__file__), 'third_party'))
+
 from PythonVoiceCodingPlugin.interface.interface import Interface
 
+# removing our dependencies from the path so as not to interfere with other packages
+sys.path.remove(os.path.join(os.path.dirname(__file__), 'third_party'))
 
 class PythonVoiceCodingPluginCommand(sublime_plugin.TextCommand):
 	def run(self, edit,arg):
