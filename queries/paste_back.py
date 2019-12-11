3f0034f4899e67f8af83a7a255ecf42ce338108f
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2019-12-11 13:11:33 +0200

    Backend pasting one alternative to another

--- queries/paste_back.py
+++ queries/paste_back.py
@@ -13,15 +13,21 @@ class PasteBack(InsertionQuery):
 			index -=1
 		if index==len(history) or history[index][1] != view_information["change_count"]:
 			return []
-		selection = history[index][2]
-		selection = selection if isinstance(selection,list) else [selection]
-		i = query_description.get("color",0)
-		print(" i.e.'s",i)
+		
 		result_text = state["result_text"]
 		alternatives_text = state["alternatives_text"]
-		output = alternatives_text[i-1] if i != 0 else result_text
+		candidates = [result_text]+alternatives_text if alternatives_text else [result_text]
+		if query_description["format"]==1:
+			selection = history[index][2]
+			selection = selection if isinstance(selection,list) else [selection]
+		if query_description["format"]==2:
+			result = state["result"]
+			alternatives = state["alternatives"]
+			candidates_location = [result]+alternatives if alternatives else [result]
+			selection = {candidates_location[query_description["color"+i]]  
+							for i in ["2","3","4"] if "color"+i in query_description}
+		output = candidates[query_description.get("color",0)]
 		surrounding = query_description.get("surrounding_punctuation",("",""))
-		print("output",output)
 		return [(x,surrounding[0]+output+surrounding[1])  for x in selection]
 
 
