466212ebfa3b1518283133ae0f0ea1adae885fa4
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2019-12-15 21:14:29 +0200

    Delete alternatives Also works with state extraction

--- queries/delete_alternatives.py
+++ queries/delete_alternatives.py
@@ -1,4 +1,5 @@
 from PythonVoiceCodingPlugin.queries.abstract import InsertionQuery,no_build_attempt
+from PythonVoiceCodingPlugin.queries.strategies import result_alternatives_sequence 
 
 
 @no_build_attempt
@@ -6,10 +7,7 @@ class DeleteAlternatives(InsertionQuery):
 	select_insertion = True
 
 	def handle_single(self,view_information,query_description,extra = {}):
-		state = extra["state"]
-		result = state["result"]
-		alternatives = state["alternatives"]
-		candidates = [result]+alternatives if alternatives else [result]
+		candidates = result_alternatives_sequence(extra["state"],location= True)
 		if query_description["format"] == 1:
 			selection = {candidates[query_description["color"+i]]  
 							for i in ["","2","3","4"] if "color"+i in query_description}
