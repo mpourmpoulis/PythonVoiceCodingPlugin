407caa726751ba0d761d70c13caf8bcd8be9e47a
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2019-12-15 19:42:55 +0200

    Paste Back now works with state extraction

--- queries/paste_back.py
+++ queries/paste_back.py
@@ -1,5 +1,5 @@
 from PythonVoiceCodingPlugin.queries.abstract import InsertionQuery,no_build_attempt
-
+from PythonVoiceCodingPlugin.queries.strategies import result_alternatives_sequence 
 
 @no_build_attempt
 class PasteBack(InsertionQuery):
@@ -13,17 +13,12 @@ class PasteBack(InsertionQuery):
 			index -=1
 		if index==len(history) or history[index][1] != view_information["change_count"]:
 			return []
-		
-		result_text = state["result_text"]
-		alternatives_text = state["alternatives_text"]
-		candidates = [result_text]+alternatives_text if alternatives_text else [result_text]
+		candidates = result_alternatives_sequence(state,text = True)
+		candidates_location = result_alternatives_sequence(state,location = True)
 		if query_description["format"]==1:
 			selection = history[index][2]
 			selection = selection if isinstance(selection,list) else [selection]
 		if query_description["format"]==2:
-			result = state["result"]
-			alternatives = state["alternatives"]
-			candidates_location = [result]+alternatives if alternatives else [result]
 			selection = {candidates_location[query_description["color"+i]]  
 							for i in ["2","3","4"] if "color"+i in query_description}
 		output = candidates[query_description.get("color",0)]
