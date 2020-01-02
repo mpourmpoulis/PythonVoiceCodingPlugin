939604f42086e9c41a08e7d0e1e7f03bd35fd042
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2020-01-02 22:16:47 +0200

    Restructure state extraction, so was to work with multiple cursors, still no use for the Level parameter

--- queries/strategies/state_extraction.py
+++ queries/strategies/state_extraction.py
@@ -1,10 +1,22 @@
-def result_alternatives_sequence(state,location=False,text = False):
+def result_alternatives_sequence(state,location=False,text = False,mode="single",level = False):
 	result_text = state["result_text"]
 	alternatives_text = state["alternatives_text"]
-	candidates_text = [result_text]+alternatives_text if alternatives_text else [result_text]
-	result_location = state["result"]
-	alternatives_location = state["alternatives"]
-	candidates_location =   [result_location] + alternatives_location if alternatives_location else [result_location]
+	if mode=="single":
+		result_text = [result_text]
+		candidates_text = result_text + alternatives_text if alternatives_text else result_text
+	elif mode=="multiple":
+		if not alternatives_text: alternatives_text = [[] for x in result_text]
+		assert len(alternatives_text)==len(result_text), "lengths are presold and alternatives must match"
+		candidates_text = [x+y for x,y in zip(result_text,alternatives_text)]
+	result = state["result"]
+	alternatives = state["alternatives"]
+	if mode=="single":
+		result = [result]
+		candidates_location = result + alternatives if alternatives else result
+	elif mode=="multiple":
+		if not alternatives: alternatives = [[] for x in result]
+		assert len(alternatives)==len(result), "lengths are presold and alternatives must match"
+		candidates_location = [x+y for x,y in zip(result,alternatives)]
 	if location and text:
 		return list(zip(candidates_location,candidates_text))
 	if location:
