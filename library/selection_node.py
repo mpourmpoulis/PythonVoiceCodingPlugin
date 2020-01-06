b50678f80bce915a1001a4c27e0f94108182f370
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2020-01-06 14:35:47 +0200

    New node for range extraction
    Still experimental but these makes  much more sense, Instead of performing a search over all possible nodes, We now simply traverse the tree from the root donned the deepest child that contains the region of interest, might need some work to make sure it works with spacing and so on and also to incorporate fake nodes.
    In general of these should make some performance improvements as the ghost now becomes Closer O(depth)

--- library/selection_node.py
+++ library/selection_node.py
@@ -18,7 +18,7 @@ def nearest_node_from_offset(root,atok,offset):
     	and  inside(s,atok.get_text_range(node))])
     return min( candidates , key= lambda y :(y[1][1]-y[1][0]) )[0]
 
-def node_from_range(root,atok, r ):
+def node_from_range_old(root,atok, r ):
 	inside = lambda x,y: (y[0]<=x[0]<y[1] and y[0]<x[1]<=y[1])
 	candidates =([(node,atok.get_text_range(node)) for node in ast.walk( root ) if not isinstance(node,ast.Module) 
     	and  inside(r,atok.get_text_range(node))])
@@ -28,3 +28,25 @@ def node_from_range(root,atok, r ):
 	print("outside note from range \n",min( candidates , key= lambda y :(y[1][1]-y[1][0]) )[0])
 
 	return min( candidates , key= lambda y :(y[1][1]-y[1][0]) )[0]
+
+
+def node_from_range_new(root,atok,r):
+    print(" inside the new note from range\n")
+    inside = lambda x,y: (y[0]<=x[0]<y[1] and y[0]<x[1]<=y[1])
+    for child in ast.iter_child_nodes(root):
+        if inside(r,atok.get_text_range(child)):
+            return node_from_range_new(child,atok,r)
+    return root
+
+
+def node_from_range(root,atok, r ):
+    # return node_from_range_old(root,atok,r)
+    return node_from_range_new(root,atok,r)
+
+
+
+
+
+
+
+
