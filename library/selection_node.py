633510bd66ab58afffdfb2d434fda57672aa0a68
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2020-01-06 17:36:01 +0200

    First experiment with fixing nodes they are visitedInside note from range

--- library/selection_node.py
+++ library/selection_node.py
@@ -1,6 +1,7 @@
 import ast
 
 from PythonVoiceCodingPlugin.third_party.asttokens import asttokens
+from PythonVoiceCodingPlugin.library.info import generic_fix 
 
 def nearest_node_from_offset(root,atok,offset):
     converter = atok._line_numbers 
@@ -31,8 +32,9 @@ def node_from_range_old(root,atok, r ):
 
 
 def node_from_range_new(root,atok,r):
-    print(" inside the new note from range\n")
+    print(" inside the new note from range\n",root)
     inside = lambda x,y: (y[0]<=x[0]<y[1] and y[0]<x[1]<=y[1])
+    generic_fix(root,atok)
     for child in ast.iter_child_nodes(root):
         if inside(r,atok.get_text_range(child)):
             return node_from_range_new(child,atok,r)
