ac696f4687d502d4c35092dacb4eda275a0c3cc8
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2020-01-11 01:37:06 +0200

    Support for selecting entire keyword ( both parameter name and value )

--- queries/argument.py
+++ queries/argument.py
@@ -30,6 +30,8 @@ class SelectArgument(SelectionQuery):
 			return make_information(get_argument_from_call,query_description["argument_index"]-1)
 		elif "keyword_index" in query_description:
 			return make_information(get_keyword_argument,query_description["keyword_index"]-1,only_keyword=True)
+		elif "entire_keyword_index" in query_description:
+			return make_information(get_keyword_argument,query_description["entire_keyword_index"]-1,only_keyword=False,only_value = False)
 		else:
 			if "sub_index" not in query_description:
 				return get_caller
