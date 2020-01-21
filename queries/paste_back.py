758cffc3efe90e60c49711552755b2b235b1123b
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2020-01-21 23:22:07 +0200

    More Formatting options for paste back

--- queries/paste_back.py
+++ queries/paste_back.py
@@ -2,6 +2,25 @@ from PythonVoiceCodingPlugin.library import make_flat
 from PythonVoiceCodingPlugin.queries.abstract import InsertionQuery,no_build_attempt
 from PythonVoiceCodingPlugin.queries.strategies import result_alternatives_sequence 
 
+def surround(surrounding,text):
+	assert isinstance(surrounding,(str,tuple,list)), "Surrounding must be either a string, tuple or list not a "  + str(type(surrounding))
+	if isinstance(surrounding,(tuple,list)):
+		surrounding = (
+			surrounding[0].replace("quotes",'"'),
+			surrounding[1].replace("quotes",'"'),
+		)
+
+		return surrounding[0] + text + surrounding[1]
+	if isinstance(surrounding,str):
+		surrounding = surrounding.replace("quotes",'"')
+		surrounding = surrounding.replace("$$",text)
+		return surrounding 
+
+
+
+
+
+
 @no_build_attempt
 class PasteBack(InsertionQuery):
 	select_insertion = True
@@ -15,7 +34,7 @@ class PasteBack(InsertionQuery):
 		candidates = result_alternatives_sequence(state,text = True)
 		candidates_location = result_alternatives_sequence(state,location = True)
 		surrounding = query_description.get("surrounding_punctuation",("",""))
-		surrounding = surrounding if surrounding != "quotes" else ('"','"')
+		# surrounding = surrounding if surrounding != "quotes" else ('"','"')
 		if query_description["format"]==1:
 			selection = state["initial_origin"]
 			if state["initial_mode"]=="multiple":
@@ -24,7 +43,7 @@ class PasteBack(InsertionQuery):
 			if state["mode"]=="single":
 				output = candidates[query_description.get("color",0)]
 				selection = selection if isinstance(selection,list) else [selection]				
-				return [(x,surrounding[0]+output+surrounding[1])  for x in selection]
+				return [(x,surround(surrounding,output))  for x in selection]
 
 			elif state["mode"]=="multiple":
 				try : 
@@ -38,7 +57,7 @@ class PasteBack(InsertionQuery):
 						output = make_flat(output)
 					except :
 						pass
-					output = ",".join([surrounding[0]+x+surrounding[1]  for x in  output])
+					output = ",".join([surround(surrounding,x)  for x in  output])
 					return [(state["initial_origin"],output)]
 					# raise Exception("can't paste multiple values the same origin!")
 
@@ -48,13 +67,13 @@ class PasteBack(InsertionQuery):
 						if len(output)==1  and isinstance(output[0],list):
 							output = make_flat(output)
 							if len(state["origin"])==1  and len(output)==len(state["initial_origin"]):
-								return [(x,surrounding[0]+y+surrounding[1])  for y,x in  zip(output,selection)]
+								return [(x,surround(surrounding,y))  for y,x in  zip(output,selection)]
 						print("their respective lengths are",len(output),len(state["initial_origin"]),output, "\n")
 						raise Exception("mismatch of things to paste and locations to place")
 
 					if any(isinstance(x,list) for x in output):
 						raise Exception("one of the results spanned over multiple selections, this is not supported!")
-					return [(x,surrounding[0]+y+surrounding[1])  for y,x in  zip(output,selection)]
+					return [(x,surround(surrounding,y))  for y,x in  zip(output,selection)]
 
 		if query_description["format"]==2:
 			if state["mode"]=="multiple":
@@ -62,7 +81,7 @@ class PasteBack(InsertionQuery):
 			selection = {candidates_location[query_description["color"+i]]  
 							for i in ["2","3","4"] if "color"+i in query_description}
 		output = candidates[query_description.get("color",0)]
-		return [(x,surrounding[0]+output+surrounding[1])  for x in selection]
+		return [(x,surround(surrounding,output))  for x in selection]
 
 
 
