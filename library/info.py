mpourmpoulis/PythonVoiceCodingPlugin

commit 0fcbc51877481349f38878509cb96068217c9694

Fix problems with sub indexing strings start with prefix r/u

library/info.py
@@ -95,7 +95,8 @@ def set_fake(root,name,fake_node):
 	fields.insert(index,fake_name)
 	root._fields = tuple(fields)
 
-
+def check_fake(root):
+	return getattr(root,"fake",False)
 	
 def get_fake(root,name):
 	return getattr(root,name + "_fake",None)
@@ -547,7 +548,27 @@ def split_string(s :str ,even_letters = True,only_first = False):
 
 def get_subparts_of_string(root,name_mode = False):
 	output = []
-	start_position = root.first_token.startpos + ( 1 if not name_mode else 0) 
+	if name_mode:
+		start_position = 0
+	else:
+		start_position = 1
+		if not check_fake(root):
+			x = root.first_token.string
+			# print("String:\n",x)
+			y1 = x.find("'")
+			y2 = x.find("\"")
+			if y1>=0 and y2>=0:
+				z = mean(y1,y2)
+			elif y1>=0:
+				z = y1
+			elif y2>=0:
+				z = y2
+			else:
+				raise Exception("problem with splitting a string , there is no beginning!")
+			start_position += z
+	start_position += root.first_token.startpos
+	# print("Start Position:\n",start_position)
+	# start_position = root.first_token.startpos + ( 1+(len(root.first_token.string) if root.first_token.type==tokenize.NAME else 0) if not name_mode else 0) 
 	original  = root.s if not name_mode else root.id
 	try :
 		splitted = split_string(root.s if not name_mode else root.id,even_letters = False if name_mode else True) 

commit 1d907c4da043295f9270692cf3736e69e9a2308a

Fixing problems with sub indexing strings volume 2, fixing typo and handling triple quoted strings
Resolves #18 fixing a typo introduced in
0fcbc51877481349f38878509cb96068217c9694 And taking Care of triple quoted strings

library/info.py
@@ -667,28 +667,29 @@ def get_subparts_of_string(root,name_mode = False):
 		start_position = 1
 		if not check_fake(root):
 			x = root.first_token.string
-			# print("String:\n",x)
 			y1 = x.find("'")
 			y2 = x.find("\"")
 			if y1>=0 and y2>=0:
-				z = mean(y1,y2)
+				z = min(y1,y2)
 			elif y1>=0:
 				z = y1
 			elif y2>=0:
 				z = y2
 			else:
 				raise Exception("problem with splitting a string , there is no beginning!")
+			try : 
+				if x[z]==x[z+1]==x[z+2]:
+					z = z + 2
+			except :
+				pass
 			start_position += z
 	start_position += root.first_token.startpos
-	# print("Start Position:\n",start_position)
-	# start_position = root.first_token.startpos + ( 1+(len(root.first_token.string) if root.first_token.type==tokenize.NAME else 0) if not name_mode else 0) 
 	original  = root.s if not name_mode else root.id
 	try :
 		splitted = split_string(root.s if not name_mode else root.id,even_letters = False if name_mode else True) 
 	except :
 		print(" exceptions were thrown")
 	index = 0
-	print("splitted ",splitted)
 	for s in splitted:
 		if not s:
 			continue