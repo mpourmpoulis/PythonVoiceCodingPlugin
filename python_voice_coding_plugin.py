bebe6f1f19ed6d01afc90db7a94db362481879ab
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2020-07-21 01:08:29 +0300

    Version bump

--- python_voice_coding_plugin.py
+++ python_voice_coding_plugin.py
@@ -11,7 +11,7 @@ sys.path.insert(0,os.path.join(os.path.dirname(__file__), 'third_party'))
 from PythonVoiceCodingPlugin.interface.interface import Interface
 
 
-PLUGIN_VERSION = (0,1,2)
+PLUGIN_VERSION = (0,1,3)
 
 settings = {}
 already_message = False	
